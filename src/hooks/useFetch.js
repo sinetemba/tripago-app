e4b8f9d2de092a9b871a8fff50f2cc244c30de83
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-08-01 18:31:12 +0200

    added loading/pending state

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,17 +2,22 @@ import {useState, useEffect} from 'react'
 
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
+        
+        setIsPending(true)
+          
           const response = await fetch(url)
           const json = await response.json()  
 
+          setIsPending(false)
           setData(json)
         }
     
         fetchData()
     }, [url])
     
-    return { data: data }
+    return { data, isPending }
 }
